f427ac87b77a81b33d52c26fec110e96932d4e44
eliteinovastaticwebsite-crypto <eliteinovastaticwebsite@gmail.com>
2025-12-09 16:49:58 +0530

    Created travel bags, duffle bags and trolley bags

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import PassportHoldersPage from './pages/PassportHoldersPage';
 import BeltsPage from './pages/BeltsPage';
 import MensBackpackPage from './pages/MensBackpack';
 import WomensBackpackPage from './pages/WomensBackpack';
+import TravelBagsPage from './pages/TravelBagPage';
+import TrolleyBagPage from './pages/TrolleyBagPage';
+import DuffleBagPage from './pages/DuffleBagPage';
 
 function App() {
   return (
@@ -22,9 +25,13 @@ function App() {
           <Route path="women-handbags" element={<WomenHandbagsPage />} />
           <Route path="laptopbags" element={<LaptopBagsPage />} />
 
+          <Route path="travelbags/leather-travel-bags" element={<TravelBagsPage />} />
+          <Route path="travelbags/trolley-bags" element={<TrolleyBagPage />} />
+          <Route path="travelbags/duffle-bags" element={<DuffleBagPage />} />
+
           <Route path="backpacks/men-backpacks" element={<MensBackpackPage />} />
           <Route path="backpacks/women-backpacks" element={<WomensBackpackPage />} />
-          
+
           <Route path="brief-cases" element={<BriefcasesPage />} />
           <Route path="wallet/menswallet" element={<MensWalletsPage />} />
           <Route path="wallet/passportholder" element={<PassportHoldersPage />} />
